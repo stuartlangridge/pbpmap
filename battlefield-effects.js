c8727b642eaf731e6c3edf536756b35f812978db
Stuart Langridge <sil@kryogenix.org>
2020-07-24 11:28:34 +0100

    style battlefield effects templates dropdown

--- battlefield-effects.js
+++ battlefield-effects.js
@@ -19,13 +19,33 @@ class BattlefieldEffects extends HTMLElement {
         bfe.container = document.createElement("div");
         bfe.container.id = "bfe";
         const styles = document.createElement("style");
-        const buttonColour = "#ccc";
+        const buttonColour = "#E4644B";
+        const buttonText = "white";
         styles.textContent = `
         #pbfe {
             text-align: right;
             margin: 0;
         }
-        #pbfe select { max-width: 4em; }
+        #pbfe select {
+            max-width: 4em;
+            font-size: 16px; /* required for ios safari to not zoom */
+            font-weight: 700;
+            color: rgba(255, 255, 255, 0.9);
+            line-height: 1.3;
+            padding: 0;
+            box-sizing: border-box;
+            margin: 0 0.5em;
+            border: 2px solid rgba(255, 255, 255, 0.9);
+            -moz-appearance: none;
+            -webkit-appearance: none;
+            appearance: none;
+            background-color: transparent;
+            background-image: url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22white%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E'), linear-gradient(to bottom, transparent 0%, transparent 100%);
+            background-repeat: no-repeat, repeat;
+            background-position: right .7em top 50%, 0 0;
+            background-size: .65em auto, 100%;
+            box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.8);
+        }
         #bfe form { /* a single BFE */
             display: grid;
             grid-gap: 2px;
@@ -36,8 +56,7 @@ class BattlefieldEffects extends HTMLElement {
         }
         #bfe form .heading {
             grid-column: 1 / span 6; grid-row: 1;
-            background: ${buttonColour};
-            color: #333;
+            color: ${buttonText};
             font-size: 9px;
             font-weight: bold;
             text-transform: uppercase;
@@ -62,10 +81,12 @@ class BattlefieldEffects extends HTMLElement {
             padding: 0.2em 0.5em;
             border-width: 0;
             background: ${buttonColour};
+            color: ${buttonText};
             min-width: 30px;
             min-height: 30px;
             position: relative;
             overflow: hidden;
+            box-shadow: none;
         }
         #bfe form span[class$='_inner'] {
             position: absolute;
@@ -89,10 +110,10 @@ class BattlefieldEffects extends HTMLElement {
             font-size: 12px;
         }
         #bfe form span.colour_inner { background-color: black; }
+        /* https://github.com/filamentgroup/select-css */
         #bfe form select {
             display: block;
             font-size: 16px; /* required for ios safari to not zoom */
-            font-family: sans-serif;
             font-weight: 700;
             color: #444;
             line-height: 1.3;
@@ -141,7 +162,7 @@ class BattlefieldEffects extends HTMLElement {
         let iv = setInterval(async () => {
             if (window.addTools) {
                 clearInterval(iv);
-                [this.toolsElement, this.toolDialogSection] = window.addTools("Battlefield effects", [pbtn, bfe.container]);
+                [this.toolsElement, this.toolDialogSection] = window.addTools("Battlefield effects", [pbtn, bfe.container], {openByDefault: true});
                 let load_effects = await this.toolsElement.load("effects");
                 if (!Array.isArray(load_effects)) load_effects = [];
                 if (load_effects.length > 0) {
