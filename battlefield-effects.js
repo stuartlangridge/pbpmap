28cb24ac77271d51f598597a5ca0e0d9b146f3f2
Stuart Langridge <sil@kryogenix.org>
2025-09-04 07:16:14 +0100

    allow picking fire of any colour

--- battlefield-effects.js
+++ battlefield-effects.js
@@ -390,6 +390,63 @@ class BattlefieldEffects extends HTMLElement {
             handleDropdown({target: html.size});
         }
 
+        function HSLToHex(hsl) {
+            const { h, s, l } = hsl;
+            const hDecimal = l / 100;
+            const a = (s * Math.min(hDecimal, 1 - hDecimal)) / 100;
+            const f = (n) => {
+            const k = (n + h / 30) % 12;
+            const color = hDecimal - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+            // Convert to Hex and prefix with "0" if required
+            return Math.round(255 * color)
+                .toString(16)
+                .padStart(2, "0");
+            };
+            return `#${f(0)}${f(8)}${f(4)}`;
+        }
+
+        function HexToHSL(hex) {
+            const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+            if (!result) {
+                throw new Error("Could not parse Hex Color");
+            }
+            const rHex = parseInt(result[1], 16);
+            const gHex = parseInt(result[2], 16);
+            const bHex = parseInt(result[3], 16);
+            const r = rHex / 255;
+            const g = gHex / 255;
+            const b = bHex / 255;
+            const max = Math.max(r, g, b);
+            const min = Math.min(r, g, b);
+            let h = (max + min) / 2;
+            let s = h;
+            let l = h;
+            if (max === min) {
+                // Achromatic
+                return { h: 0, s: 0, l };
+            }
+            const d = max - min;
+            s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+            switch (max) {
+                case r:
+                    h = (g - b) / d + (g < b ? 6 : 0);
+                    break;
+                case g:
+                    h = (b - r) / d + 2;
+                    break;
+                case b:
+                    h = (r - g) / d + 4;
+                    break;
+            }
+            h /= 6;
+            s = s * 100;
+            s = Math.round(s);
+            l = l * 100;
+            l = Math.round(l);
+            h = Math.round(360 * h);
+            return { h, s, l };
+        }
+
         this.EFFECTS = {
             CIRCLE: (ctx, mainColour, radiusSquares, opacity, cxp, cyp, squareSize, done) => {
                 let rg = ctx.createRadialGradient(cxp + squareSize / 2, cyp + squareSize / 2, 0,
@@ -422,10 +479,24 @@ class BattlefieldEffects extends HTMLElement {
                 const boxw = squareSize * (radiusSquares * 2 + 1);
                 const M = boxw / 32; // circles are in a 32x32 box
 
+                // feed it orange, you get red and yellow as dark and light
+                const midColourHSL = HexToHSL(mainColour);
+                const darkColourHSL = {
+                    h: midColourHSL.h - 30,
+                    s: midColourHSL.s,
+                    l: midColourHSL.l
+                }
+                if (darkColourHSL.h < 0) darkColourHSL.h = 360 + darkColourHSL.h;
+                const lightColourHSL = {
+                    h: (midColourHSL.h + 30) % 360,
+                    s: midColourHSL.s,
+                    l: midColourHSL.l
+                }
+
                 const COLS = [
-                    "#ff0000" + opacityHex,
-                    "#ff7000" + opacityHex,
-                    "#ffd700" + opacityHex 
+                    HSLToHex(darkColourHSL) + opacityHex,
+                    HSLToHex(midColourHSL) + opacityHex,
+                    HSLToHex(lightColourHSL) + opacityHex 
                 ];
 
                 function R(min, max) {
@@ -446,7 +517,6 @@ class BattlefieldEffects extends HTMLElement {
                         const cy = boxy + (drawy * M);
                         const cr = r * M;
 
-                        console.log({drawx, drawy})
                         ctx.fillStyle = col;
                         ctx.beginPath();
                         ctx.arc(cx, cy, cr, 0, 2*3.1415);
